3ce831f795e43984b4f09fdf13bc30e43344f363
John Devine <john.h.devine@gmail.com>
2023-05-30 16:43:38 +0700

    Working well

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,49 @@
-#include <JDGlobal.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// See https://diyi0t.com/ina219-tutorial-for-arduino-and-esp/ for more info and wiring
+
+#include <JDGlobal.h>
+// #include "Wire.h"
+#include "Adafruit_INA219.h"
+
+Adafruit_INA219 ina219;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Open serial communications and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  if (! ina219.begin()) {
+    Serial.println("Failed to find INA219 chip");
+    while (1) { delay(10); }
+  }
+
+  Serial.print("BV"); Serial.print("\t"); // Bus Voltage
+  Serial.print("SV"); Serial.print("\t"); // Shunt Voltage
+  Serial.print("LV"); Serial.print("\t"); // Load Voltage
+  Serial.print("C"); Serial.print("\t");  // Current
+  Serial.println("P");  // Power
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  float shuntvoltage = 0;
+  float busvoltage = 0;
+  float current_mA = 0;
+  float loadvoltage = 0;
+  float power_mW = 0;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  shuntvoltage = ina219.getShuntVoltage_mV();
+  busvoltage = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  power_mW = ina219.getPower_mW();
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
+
+  Serial.print(busvoltage); Serial.print("\t"); 
+  Serial.print(shuntvoltage); Serial.print("\t");
+  Serial.print(loadvoltage); Serial.print("\t");
+  Serial.print(current_mA); Serial.print("\t");
+  Serial.println(power_mW);
+
+  delay(1000);
 }
